15f14bd7c33bbfd98bca8a84efa6da67f11f1480
Dave Watt <gonebikn@gmail.com>
2017-01-14 13:48:09 -0800

    PopTart last changes with a good understanding now :)

--- src/net/wattwire/Inherit_Main.java
+++ src/net/wattwire/Inherit_Main.java
@@ -4,27 +4,24 @@ import net.wattwire.dataModel.Female;
 import net.wattwire.dataModel.Human;
 import net.wattwire.dataModel.Male;
 
-import static net.wattwire.TalkAsHuman.talkashuman;
-
 public class Inherit_Main {
 
     public static void main(String[] args) {
 	// *** Blow me!
 
-        Human h1,h2;
+        Human h,h1,h2;
+
+        h = new Human("Pat", "ambiguous");
+        System.out.println("Human h is "+h.getGender()+" and says \"" + h.speak() + "\"");
 
         Male m = new Male("Mike", "male", 3.9f);
         Female f = new Female("Barbie","female",9.0f);
 
-
         h1 = m;
-        h2 = f;
-
-        h2 = h1;
+        h2 = (Human)f;
 
         System.out.println("Human h1 is "+h1.getGender()+" and says \"" + h1.speak() + "\"");
         System.out.println("Human h2 is "+h2.getGender()+" and says \"" + h2.speak() + "\"");
-
 
     }
 }
